a1bdd174bf031294bb9fbdf1c45847fba758c3c0
harireddy7 <harimsd8@gmail.com>
2024-08-05 20:39:05 +0530

    add preview workflow

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>Cloud Run Demo</p>
-        <small>(v1.0.2)</small>
+        <small>preview v1</small>
       </header>
     </div>
   );
