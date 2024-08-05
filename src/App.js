e3dddd6e6c46c3ee884cad33abc0f59dac1c642d
harireddy7 <harimsd8@gmail.com>
2024-08-05 22:28:59 +0530

    add deploy container port to preview and release workflow

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>Cloud Run Demo</p>
-        <small>preview v2</small>
+        <small>preview v3</small>
       </header>
     </div>
   );
