6df655830d03a9faef110fddbafe3097344880eb
harireddy7 <harimsd8@gmail.com>
2024-08-09 16:24:39 +0530

    add cleanup workflow to delete service and image post merging

--- src/App.js
+++ src/App.js
@@ -5,11 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>Cloud Run Demo</p>
-        <small>Using gcloud commands to build and deploy</small>
-        <div style={{ marginTop: '2rem', fontSize: '1.2rem' }}>
-          <div>REACT_APP_BE_API_URL: {process.env.REACT_APP_BE_API_URL}</div>
-          <div>REACT_APP_ENV: {process.env.REACT_APP_ENV}</div>
-        </div>
+        <small>Using helm template and gcloud sdk to deploy to cloud run</small>
       </header>
     </div>
   );
