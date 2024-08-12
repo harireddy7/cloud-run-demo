9ea6bea5f222f9ad866e9f3599f017e60a8a3ce0
harireddy7 <harimsd8@gmail.com>
2024-08-12 18:22:18 +0530

    build publish deploy preview image to cloud run preview service

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
       <header className="App-header">
         <p>Cloud Run Demo</p>
         <small>Add and update pullrequest, release, cleanup and preview workflows</small>
-        <small>Preview workflow</small>
+        <small>Preview workflow check 1</small>
       </header>
     </div>
   );
