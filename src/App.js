9cf7907a3dac2438fa82bf713a1ec801315865a5
harireddy7 <harimsd8@gmail.com>
2024-08-14 14:58:09 +0530

    test label event on new commit to pr

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
       <header className="App-header">
         <p>Cloud Run Demo</p>
         <small>Add and update pullrequest, release, cleanup and preview workflows</small>
-        <small>Test pullrequest and label event workflow 2</small>
+        <small>Test pullrequest and label event workflow 3</small>
       </header>
     </div>
   );
