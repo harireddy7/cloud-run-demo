b38c8d1e8003eb1f9f0bd88553c49637b37253a7
harireddy7 <harimsd8@gmail.com>
2024-08-27 16:59:11 +0530

    test release 1 with project 2 cloud run

--- src/App.js
+++ src/App.js
@@ -7,6 +7,12 @@ function App() {
         <p>Cloud Run Demo</p>
         <small>Add and update pullrequest, release, cleanup and preview workflows</small>
         <small>deploy image from project 1 with cloud run service in project 2 (github actions)</small>
+        <ol>
+          <li>Add project 1 authenticated user account with cloud run admin permission to project 2 IAM</li>
+          <li>Create a service account in project 2</li>
+          <li>Grant project 1 account the cloud run service agent access in the project 2 service account</li>
+          <li>Add project 2 service account access to Artifact Registry Reader role in project 1</li>
+        </ol>
       </header>
     </div>
   );
