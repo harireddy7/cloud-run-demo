453f02a1a9a4a767135388db3e96f93daf567c5c
harireddy7 <harimsd8@gmail.com>
2024-08-27 16:06:51 +0530

    test cloud run deployment from project 2

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
       <header className="App-header">
         <p>Cloud Run Demo</p>
         <small>Add and update pullrequest, release, cleanup and preview workflows</small>
-        <small>deploy image from project 1 with cloud run service in project 2</small>
+        <small>deploy image from project 1 with cloud run service in project 2 (github actions)</small>
       </header>
     </div>
   );
