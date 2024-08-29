ef36c245709a4b72d6dd400cdad063e25bda009a
harireddy7 <harimsd8@gmail.com>
2024-08-29 07:51:26 +0530

    update release workflow

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
 import './App.css';
 
 function App() {
+  console.log(process.env.NODE_ENV, process.env.REACT_APP_VERSION)
   return (
     <div className="App">
       <header className="App-header">
-        <p>Cloud Run Demo</p>
-        <small>Add and update pullrequest, release, cleanup and preview workflows</small>
-        <small>deploy image from project 1 with cloud run service in project 2 (github actions)</small>
-        <ol>
-          <li>Add project 1 authenticated user account with cloud run admin permission to project 2 IAM</li>
-          <li>Create a service account in project 2</li>
-          <li>Grant project 1 account the cloud run service agent access in the project 2 service account</li>
-          <li>Grant project 1 account with cloud run admin access in project 2 IAM</li>
-          <li>Add project 2 service account access to Artifact Registry Reader role in project 1</li>
-        </ol>
-
-        <div>Auth with both accounts</div>
+        <p>Cloud Run Demo - Release - {process.env.REACT_APP_VERSION} ({process.env.NODE_ENV}) </p>
       </header>
     </div>
   );
